d69af79b73b65fbc5f6d9d2e07a37ed6d06db6ba
Jose43215 <38092766+Jose43215@users.noreply.github.com>
2021-08-16 17:51:59 -0500

    Update AF.cpp

--- Practica1/AF.cpp
+++ Practica1/AF.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cstring>
 
+class Af{
+    int numcaracteres;
+    int tamCadena;
+    int numcarac;
+    int tamCadena;
+    int numEstados;
+    int edoInicial;
+    int edoFinal;
+    int numSimbolos;
+    int a;
+    int q;
+    char c;
+    char cadena[100];
+    char desSimbolos[100];
+    char ruta[50];
+  public:
+    void mostrar();
+    void mostrarTabla();
+    void proceso(char[]);
+    void obtenerdato();
+    char *obtenerCadena();
+};
+
+int main(){
+  Af AFD;
+  Af AFN;
+  
+  return 0;
+}
